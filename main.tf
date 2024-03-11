39da6a74cbac9c749109aa8b15ef62def8b1b23c
Daniel Pereira <dan.pereira@hotmail.com>
2024-03-10 23:02:56 -0300

    initial commit terraform

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24" # Altere o bloco de CIDR conforme necessário
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a" # Altere a zona de disponibilidade conforme necessário
+  availability_zone = ["us-east-1a","us-east-1a"] # Altere a zona de disponibilidade conforme necessário
 }
 
 # Security group
